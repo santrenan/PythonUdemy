4ac9abe1e7cb1a2ec5727785b5e89b4597ced186
santrenan <renansant939@gmail.com>
2025-04-13 07:32:17 -0300

    Imc

---
+++ aula12.py
@@ -0,0 +1,8 @@
+nome = "sanitty"
+altura = 1.79
+peso = 58.5
+imc = peso / (altura ** 2)
+
+print(f"{nome} tem {altura:.2f} de altura")
+print(f"pesa {peso} quilos e seu imc é")
+print(f"{imc:.2f}")
